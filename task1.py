d2a60585f306dfb7ef4e01caa570724519cb1567
keitero <116052232+keitero@users.noreply.github.com>
2023-10-03 08:43:10 -0400

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,18 @@
+import pandas as pd #import the "tool" pandas to read excel file from the computer
+
+wb = pd.read_excel("23P1-M010-Group12.xlsx", dtype = str)# read the excel file according to the excel name
+
+c = list(wb["characters"])#create a list for all the characters
+b = list(wb["binary"])#create a list for all the binarys
+
+try:
+    i = c.index("\\n")
+    c[i] = "\n"
+except:
+    print("\\n was not found")# try to fix \\n into \n, if there is no \\n then skip
+    
+try:
+    i = c.index("<space>")
+    c[i] = " "
+except:
+    print("<space> was not found")# try to fix <space> into " ", if there is no <space> then skip
