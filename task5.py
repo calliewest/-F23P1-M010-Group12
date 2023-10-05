9b494006b2e13de1216646b2bafbffc2313d9b5c
DA2458 <98161160+DA2458@users.noreply.github.com>
2023-10-05 16:15:52 -0400

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,5 @@
+# open binoutput.txt
+# store the text in the file into a string variable after splitting at the "."
+# close binoutput.txt
+# use task 3 functions to convert from binary to text
+# put the result of conversion into textoutput.txt
